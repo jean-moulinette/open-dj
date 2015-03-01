9df964751bf9e912878a8eb7371524c88d77dd97
Fanghornn <fanghornn@gmail.com>
2015-03-01 21:44:29 +0100

    Le traitement de la réponse de youtube(traverse) se fait maintenant coté client

--- public/js/index.js
+++ public/js/index.js
@@ -6,8 +6,11 @@ socket.on('connect', function(data){
 
 socket.on('yt-result', function(data){
 
+	//Filtrage de la réponse pour ne ressorir uniquement les élements intéréssants
+	data = getHtmlResult(data);
+
 	$('#search-yt-results').html(data);
-	
+
 	$('a').click(function(){
 
 		if(typeof this.href.split('v=')[1] == 'undefined'){
@@ -31,6 +34,41 @@ socket.on('yt-result', function(data){
 
 $('#launch-search').click(launchYtSearch);
 
+/**
+ *	getHtmlResult
+ *
+ *	@param: { string } - Le code html renvoyé par le serveur node
+ *
+ *	@return: { string } - Le code HTML de la réponse épurée
+ */
+function getHtmlResult(data){
+
+	var htmlNinja = '';
+
+	var garbageDom = $.parseHTML(data);
+
+	$(garbageDom).find('.yt-lockup').each(function(){
+
+		$(this).find('button').each(function(){
+			$(this).remove();
+		});
+
+		var linkImage = $(this).find('.yt-lockup-thumbnail').html();
+		var title = $(this).find('.yt-lockup-content').find('a').html();
+		
+		var singleResult = linkImage+title+'<br/><br/><br/>';
+
+		htmlNinja += singleResult;
+	});
+
+	return htmlNinja;
+}
+
+/**
+ *	launchYtSearch
+ *
+ *	Fonction appellé au clic sur le bouton de recherche
+ */
 function launchYtSearch(){
 
 	var inputVal = $('#yt-search').val();
