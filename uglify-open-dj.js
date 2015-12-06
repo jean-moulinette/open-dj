e5eb83ad2339475d104af46652cfa307991798a1
fanghornn <fanghornn@gmail.com>
2015-12-06 23:46:54 +0100

    Ajout d'une task pour compresser les .css

--- uglify-open-dj.js
+++ uglify-open-dj.js
@@ -1,7 +1,11 @@
-var UglifyJS = require('uglify-js');
 var fs = require('fs');
 
-var result = UglifyJS.minify(["public/js/YoutubePlayer.js", "public/js/SocketsManager.js"], {
+/**
+ * JS
+ */
+var UglifyJS = require('uglify-js');
+
+var resultJS = UglifyJS.minify(["public/js/YoutubePlayer.js", "public/js/SocketsManager.js"], {
 	mangle: true,
 	compress: {
 		sequences: true,
@@ -14,5 +18,34 @@ var result = UglifyJS.minify(["public/js/YoutubePlayer.js", "public/js/SocketsMa
 		drop_console: true
 	}
 });
+console.log('\nUglyfied JS : :');
+console.log('\n');
 
-fs.writeFileSync('./public/js/Build/open.dj.min.js', result.code);
+console.log(resultJS.code);
+
+var jsPath = './public/js/Build/open.dj.min.js';
+
+fs.writeFileSync(jsPath, resultJS.code);
+
+console.log('\nSauvegarde réussie. ' + jsPath);
+
+/**
+ * CSS
+ */
+var uglifycss = require('uglifycss');
+
+var uglified = uglifycss.processFiles(
+    [ './public/css/open-dj.css'],
+    { maxLineLen: 500, expandVars: true }
+);
+
+var cssPath = './public/css/Build/open-dj.min.css';
+
+fs.writeFileSync(cssPath, uglified);
+
+console.log('\n\n');
+console.log('Uglyfied CSS : :');
+console.log('\n');
+console.log(uglified);
+
+console.log('\n\nSauvegarde réussie. ' + cssPath+'\n');
