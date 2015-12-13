2b43b92c3360a0127564d1832e31260608f1d48e
Jean-Baptiste PRIAM MASSAT <jb.priam-massat@geomatika.fr>
2015-12-13 13:55:24 +0100

    Mise en place d'un port par défaut dans la conf de base

--- open-dj.conf.js
+++ open-dj.conf.js
@@ -3,8 +3,8 @@ var config =  {};
 //Set local ip or dns of your server
 config.host = '';
 
-//Set port
-config.port = '';
+//Set port (default is 1337)
+config.port = '1337';
 
 //set dev boolean value (if false, minified scripts will load)
 config.dev = false;
